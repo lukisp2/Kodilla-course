9a7ca5009f7b3fa4db12ab7347776a5103497b87
Lukasz MARCHEL <lukaszmarchelelk@gmail.com>
2022-03-09 07:11:46 +0100

    Commit zadania 1 i 2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        simpleUser.add(3, 3);
+
+
+        if (result.equals("theForumUser")) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int a =5;
+        int b =6;
+
+        Calculator calculator = new Calculator();
+      if(calculator.add(a,b) == a+b){
+          System.out.println("Test dodawania ok");
+        }  else {System.out.println("Test dodawania nie ok");}
+
+        if(calculator.subtract(a,b) == a-b){
+            System.out.println("Test odejmowania ok");
+        }  else {System.out.println("Test odejmowania nie ok");}
+
+
+   
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -0,0 +1,20 @@
+package com.kodilla.testing.user;
+
+
+public class SimpleUser {
+
+    private String username;
+
+    public SimpleUser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int add(int a, int b){
+
+        return a+b;
+    }
+}
